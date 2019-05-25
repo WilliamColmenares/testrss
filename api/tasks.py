ff503daa2bc27e8536ecdf0ec691b1f94886bb68
William Colmenares <colmenares.william@gmail.com>
2019-05-24 21:07:26 -0400

    fix typho entities -> entries

--- api/tasks.py
+++ api/tasks.py
@@ -7,13 +7,13 @@ import re
 @shared_task()
 def get_news():
     rssnews = feedparser.parse('https://news.google.com/rss')  # Google's rss
-    for i in range(len(rssnews['entities'])):
-        news_id = int(rssnews['entities'][i].id)
+    for i in range(len(rssnews['entries'])):
+        news_id = int(rssnews['entries'][i].id)
         try:
             News.objects.get(id=news_id)
         except News.DoesNotExist:
             News.objects.create(
                 id=news_id,
-                title=rssnews['entities'][i].title,
+                title=rssnews['entries'][i].title,
                 # Here I user this regex to remove some html tags
                 content=re.sub('<[^<]+?>', '', rssnews['entries'][0].summary))
